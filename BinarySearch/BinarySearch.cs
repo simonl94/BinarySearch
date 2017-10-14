826d18afe89ff6001fc8e6b122b285e53e97a19c
Simon Lee <slee1994@hotmail.com>
2017-10-14 17:36:10 +1100

    Almost completed
    Currently returns position '3' no matter what. Probably a logic problem in Program.cs. Otherwise essentially done.

---
+++ BinarySearch/BinarySearch.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearch
+{
+    class BinaryArray
+    {
+        public int[] array; //array of values
+        public static Random randNum = new Random();
+
+        public BinaryArray()
+        {
+            array = new int[10]; //array length
+
+            // fill array with random numbers
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = randNum.Next(0, 100);
+            }
+
+            Array.Sort(array);
+        }
+
+
+        public int findValue(int searchElement)
+        {
+            int lower = 0;
+            int upper = array.Length - 1;
+            int value = -1;
+            int middle = (lower + upper - 1) / 2;
+
+            do
+            {
+                // Print remaining elements in an array
+                Console.WriteLine(RemainingElements(lower, upper));
+         
+                for (int i = 0; i < middle; i++)
+                    Console.WriteLine("  ");
+                   
+                Console.WriteLine(" * "); //Shows current middle
+                
+                //If the element is found at middle
+                if (searchElement == array[middle])
+                    value = middle;
+
+                else if (searchElement < array[middle])
+                {
+                    value = middle - 1; //Elminates lower half
+                }
+                else
+                {
+                    lower = middle + 1; //Elminates lower half
+
+                    middle = (lower + upper + 1) / 2; //Re-calculates middle
+                }
+            } while ((lower <= upper) && (value == -1));
+
+            return value;
+            
+        }
+
+        public string RemainingElements(int lower, int upper)
+        {
+            string temporary = string.Empty;
+
+            // output spaces for alignment
+            for (int i = 0; i < lower; i++)
+                temporary += "    ";
+
+            // output elements left in array 
+            for (int i = lower; i <= upper; i++)
+                temporary += array[i] + " ";
+
+            temporary += "\n";
+            return temporary;
+        }
+
+        public override string ToString()
+        {
+            return RemainingElements(0, array.Length - 1);
+        }
+    }
+}
---
+++ BinarySearch/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearch
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // create array and output it
+            BinaryArray searchArray = new BinaryArray();
+            Console.WriteLine(searchArray);
+
+            // prompt and input first int from user
+            Console.Write("Please enter an integer value (-1 to quit): ");
+            var searchInt = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            // repeatedly input an integer; -1 terminates the application
+            while (searchInt != -1)
+            {
+                // use binary search to try to find integer
+                var position = searchArray.findValue(searchInt); // location of search key in array
+
+                // return value of -1 indicates integer was not found
+                if (position == -1)
+                    Console.WriteLine("The integer {0} was not found.\n", searchInt);
+                else
+                    Console.WriteLine("The integer {0} was found in position {1}.\n", searchInt, position);
+
+                // Get next input from user
+                Console.WriteLine("Please enter an integer value (-1 to quit): ");
+                searchInt = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
